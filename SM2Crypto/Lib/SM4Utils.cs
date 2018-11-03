c6aaa9bb276b32b5564affbd3e899cf30a182b81
njk888 <564972203@qq.com>
2018-11-04 01:25:28 +0800

    重新调式SM2加密和解密算法，增加了Hex.encode() 和Hex.decode()

---
+++ SM2Crypto/Lib/SM4Utils.cs
@@ -0,0 +1,126 @@
+﻿using Org.BouncyCastle.Utilities.Encoders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SM2Crypto.Lib
+{
+    class SM4Utils
+    {
+        public String secretKey = "";
+        public String iv = "";
+        public bool hexString = false;
+        public byte[] secretKeyBuff;
+
+        public String Encrypt_ECB(String plainText)
+        {
+            SM4_Context ctx = new SM4_Context();
+            ctx.isPadding = true;
+            ctx.mode = SM4.SM4_ENCRYPT;
+
+            byte[] keyBytes;
+            if (hexString)
+            {
+                keyBytes = Hex.Decode(secretKey);
+            }
+            else
+            {
+                keyBytes = Encoding.ASCII.GetBytes(secretKey);
+            }
+
+            SM4 sm4 = new SM4();
+            sm4.sm4_setkey_enc(ctx, keyBytes);
+            byte[] encrypted = sm4.sm4_crypt_ecb(ctx, Encoding.ASCII.GetBytes(plainText));
+
+            String cipherText = Encoding.ASCII.GetString(Hex.Encode(encrypted));
+            return cipherText;
+        }
+
+        public byte[] Encrypt_ECB(byte[] plainBytes, byte[] keyBytes)
+        {
+            SM4_Context ctx = new SM4_Context();
+            ctx.isPadding = false;
+            ctx.mode = SM4.SM4_ENCRYPT;
+
+            SM4 sm4 = new SM4();
+            sm4.sm4_setkey_enc(ctx, keyBytes);
+            byte[] encrypted = sm4.sm4_crypt_ecb(ctx, plainBytes);
+            return encrypted;
+
+            //return Hex.Encode(encrypted);
+        }
+
+        public String Decrypt_ECB(String cipherText)
+        {
+            SM4_Context ctx = new SM4_Context();
+            ctx.isPadding = true;
+            ctx.mode = SM4.SM4_DECRYPT;
+
+            byte[] keyBytes;
+            if (hexString)
+            {
+                keyBytes = Hex.Decode(secretKey);
+            }
+            else
+            {
+                keyBytes = Encoding.ASCII.GetBytes(secretKey);
+            }
+
+            SM4 sm4 = new SM4();
+            sm4.sm4_setkey_dec(ctx, keyBytes);
+            byte[] decrypted = sm4.sm4_crypt_ecb(ctx, Hex.Decode(cipherText));
+            return Encoding.ASCII.GetString(decrypted);
+        }
+        public String Encrypt_CBC(String plainText)
+        {
+            SM4_Context ctx = new SM4_Context();
+            ctx.isPadding = true;
+            ctx.mode = SM4.SM4_ENCRYPT;
+
+            byte[] keyBytes;
+            byte[] ivBytes;
+            if (hexString)
+            {
+                keyBytes = Hex.Decode(secretKey);
+                ivBytes = Hex.Decode(iv);
+            }
+            else
+            {
+                keyBytes = Encoding.ASCII.GetBytes(secretKey);
+                ivBytes = Encoding.ASCII.GetBytes(iv);
+            }
+
+            SM4 sm4 = new SM4();
+            sm4.sm4_setkey_enc(ctx, keyBytes);
+            byte[] encrypted = sm4.sm4_crypt_cbc(ctx, ivBytes, Encoding.ASCII.GetBytes(plainText));
+
+            String cipherText = Encoding.ASCII.GetString(Hex.Encode(encrypted));
+            return cipherText;
+        }
+
+        public String Decrypt_CBC(String cipherText)
+        {
+            SM4_Context ctx = new SM4_Context();
+            ctx.isPadding = true;
+            ctx.mode = SM4.SM4_DECRYPT;
+
+            byte[] keyBytes;
+            byte[] ivBytes;
+            if (hexString)
+            {
+                keyBytes = Hex.Decode(secretKey);
+                ivBytes = Hex.Decode(iv);
+            }
+            else
+            {
+                keyBytes = Encoding.ASCII.GetBytes(secretKey);
+                ivBytes = Encoding.ASCII.GetBytes(iv);
+            }
+
+            SM4 sm4 = new SM4();
+            sm4.sm4_setkey_dec(ctx, keyBytes);
+            byte[] decrypted = sm4.sm4_crypt_cbc(ctx, ivBytes, Hex.Decode(cipherText));
+            return Encoding.ASCII.GetString(decrypted);
+        }
+    }
+}
---
+++ SM2Crypto/Program.cs
@@ -0,0 +1,67 @@
+﻿using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Math.EC;
+using Org.BouncyCastle.Utilities.Encoders;
+using SM2Crypto.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM2Crypto
+{
+    class Program
+    {
+        private static  string PubKey= "041E353292615666BB47F6358D3E893394D34AF30D64875E2E422182C15885D3ECA697C345EED99268D3CAC5F6054780C34433E1BF12EBFF1F744B67A2F6863CFB";
+        private static  string PriKey = "00FAB34B54C026D158B54C88BC0463CB79B22661C7C870AD2A0455300E05471CE1";
+
+
+        static void Main(string[] args)
+        {
+
+            //byte a = 149;
+            //var a1 = Hex.Encode(new byte[] { a });
+            //var b = Hex.Decode(a1);
+
+            //生成公钥私钥对
+            TestSm2GetKeyPair();
+
+            //test
+            TestSm2Enc();
+
+            Console.WriteLine("finish work");
+            Console.ReadKey();
+        }
+
+        public static void TestSm2GetKeyPair()
+        {
+            SM2Utils sm2Utils = new SM2Utils();
+            ECPoint pubk;
+            BigInteger prik;
+            SM2Utils.GenerateKeyPair( out pubk,  out prik);
+            PubKey = Encoding.ASCII.GetString(Hex.Encode(pubk.GetEncoded())).ToUpper();
+            PriKey = Encoding.ASCII.GetString(Hex.Encode(prik.ToByteArray())).ToUpper();
+            //System.Console.Out.WriteLine("公钥: " + Encoding.ASCII.GetString(Hex.Encode(publicKey.GetEncoded())).ToUpper());
+            //System.Console.Out.WriteLine("私钥: " + Encoding.ASCII.GetString(Hex.Encode(privateKey.ToByteArray())).ToUpper());
+        }
+
+
+        public static  void TestSm2Enc()
+        {
+            string testStr = "hello world";
+            Console.WriteLine("原始数据 : " + testStr);
+            byte[] sourceData = Encoding.ASCII.GetBytes(testStr);
+            byte[] pubk = Encoding.ASCII.GetBytes(PubKey);
+            string encStr =  SM2Utils.Encrypt(Hex.Decode(pubk), sourceData);
+
+            Console.WriteLine("加密后数据 : " + encStr);
+
+            byte[] prik = Encoding.ASCII.GetBytes(PriKey);
+            var data =Hex.Decode(Encoding.ASCII.GetBytes(encStr));
+            var decodedData = SM2Utils.Decrypt(Hex.Decode(prik), data);
+
+            var decodedStr = Encoding.ASCII.GetString(decodedData);
+            Console.WriteLine("解密后数据 : " + decodedStr);
+        }
+    }
+}
